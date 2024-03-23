54976162d50939c497e05b1ef54451e9e8cf3401
alifzadanir <alifadanir@gmail.com>
2024-03-23 11:37:21 +0700

    menambahkan class SayaTubeVidio

--- tpmodul6_1302220114/Program.cs
+++ tpmodul6_1302220114/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        this.id = new Random().Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int increaseAmount)
+    {
+        playCount += increaseAmount;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("ID: {0}", id);
+        Console.WriteLine("Judul: {0}", title);
+        Console.WriteLine("Jumlah Tayangan: {0}", playCount);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Masukkan nama praktikan
+        string namaPraktikan = "Alif Adani Rahmat";
+
+        var video = new SayaTubeVideo("Tutorial Design By Contract - " + namaPraktikan);
+        video.PrintVideoDetails();
+
+        video.IncreasePlayCount(10);
+        video.PrintVideoDetails();
+    }
+}
